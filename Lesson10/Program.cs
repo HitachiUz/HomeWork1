b364d7d36a27ec8936b6088085dd7acafe138fb3
ne4eHbKa 3 <hardblock01@gmail.com>
2022-10-07 16:37:04 +0300

    completed Lesson10

---
+++ Lesson10/Program.cs
@@ -0,0 +1,14 @@
+﻿/*10. Напишите программу, котороя на вход принимает 3-ёх значное число и на выходе показывает вторую цифру этого числа
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите чило");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 1000 && number > 99)
+{
+    Console.WriteLine(number/10 %10);
+}
+else
+    Console.WriteLine("Введенно не 3-ёх значное число");
